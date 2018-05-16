7926f5129e08f47d4bb65854ae2047ed6d836056
Unknown <axel.uran@epfl.ch>
2018-05-16 10:11:00 +0200

    Introduced Freezing and Splitted training

--- main2.py
+++ main2.py
@@ -2,7 +2,7 @@ import matplotlib
 
 matplotlib.use('Agg')
 from helper import prepare_data_standard_from_list, load_simplify_data
-from controllers import full_distributed, incremental_distributed_learning
+from controllers import freezing_layers, splitted_layers
 import glob as glob
 
 train_list = glob.glob('../../../opt/shared-data/BCI_Comp_4/A*E.mat')
@@ -13,21 +13,36 @@ test_list = glob.glob('../../../opt/shared-data/BCI_Comp_4/A*T.mat')
 
 train_list.sort()
 test_list.sort()
+#
+#
+# # Distributed learning
+# big_X_train, big_y_train = prepare_data_standard_from_list(load_simplify_data(train_list, True, False))
+# big_X_test, big_y_test = prepare_data_standard_from_list(load_simplify_data(test_list, True, False))
+#
+# full_distributed('EEG_net', big_X_train, big_y_train, big_X_test, big_y_test,
+#                  class_names=['Left hand', 'Right hand','Both Feet', 'Tongue'],
+#                  ch_num=25, dr=0.1, addon='_01_Dropout')
+#
+# # Distributed learning
+# big_X_train, big_y_train = prepare_data_standard_from_list(load_simplify_data(train_list, True, False))
+# big_X_test, big_y_test = prepare_data_standard_from_list(load_simplify_data(test_list, True, False))
+#
+# full_distributed('EEG_net', big_X_train, big_y_train, big_X_test, big_y_test,
+#                  class_names=['Left hand', 'Right hand','Both Feet', 'Tongue'],
+#                  ch_num=25, dr=0.2, addon='_02_Dropout')
 
 
-
-# Distributed learning
+# Freezing Learning
 big_X_train, big_y_train = prepare_data_standard_from_list(load_simplify_data(train_list, True, False))
 big_X_test, big_y_test = prepare_data_standard_from_list(load_simplify_data(test_list, True, False))
 
-full_distributed('EEG_net', big_X_train, big_y_train, big_X_test, big_y_test,
+freezing_layers('EEG_net', big_X_train, big_y_train, big_X_test, big_y_test,
                  class_names=['Left hand', 'Right hand','Both Feet', 'Tongue'],
                  ch_num=25, dr=0.1, addon='_01_Dropout')
 
-# Distributed learning
-big_X_train, big_y_train = prepare_data_standard_from_list(load_simplify_data(train_list, True, False))
-big_X_test, big_y_test = prepare_data_standard_from_list(load_simplify_data(test_list, True, False))
-
-full_distributed('EEG_net', big_X_train, big_y_train, big_X_test, big_y_test,
+# Splitted Learning
+splitted_layers('EEG_net', big_X_train, big_y_train, big_X_test, big_y_test,
                  class_names=['Left hand', 'Right hand','Both Feet', 'Tongue'],
-                 ch_num=25, dr=0.2, addon='_02_Dropout')
+                 ch_num=25, dr=0.1, addon='_01_Dropout')
+
+# TODO: See variations in performance with different number of frozen layers
